3a3d7052ca3f1ba34de5d708fd5830855c3f29d6
Hprogram <wjs2096@gmail.com>
2023-01-02 18:21:34 +0900

    httpModule, Logger 추가

--- src/bus/bus.module.ts
+++ src/bus/bus.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { Logger, Module } from '@nestjs/common';
 import { BusController } from './bus.controller';
 import { BusService } from './bus.service';
 
 @Module({
+  imports: [HttpModule],
   controllers: [BusController],
-  providers: [BusService]
+  providers: [BusService, Logger],
 })
 export class BusModule {}
